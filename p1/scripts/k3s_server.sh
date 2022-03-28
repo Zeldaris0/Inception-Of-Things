edd1551c4c34d38b95a22c3449d9d8952331b632
Mouad El Azami <mouadelazami0@gmail.com>
2022-03-28 15:34:41 +0000

    p1 done

--- p1/scripts/k3s_server.sh
+++ p1/scripts/k3s_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 yum install -y net-tools
-systemctl disable firewalld --now
-curl -sfL https://get.k3s.io | sh -s - --flannel-iface=eth1 --write-kubeconfig-mode 644
+
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface eth1" sh -s -
 cat /var/lib/rancher/k3s/server/node-token  > /config/TOKEN_FILE
--- p1/scripts/k3s_worker.sh
+++ p1/scripts/k3s_worker.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 yum install -y net-tools
-systemctl disable firewalld --now
-
-curl -sfL https://get.k3s.io | sh -s - --flannel-iface=eth1 K3S_URL=https://192.168.42.110:6443 K3S_TOKEN_FILE=\"/config/TOKEN_FILE\"
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" K3S_URL=https://192.168.42.110:6443 K3S_TOKEN=$(cat /config/TOKEN_FILE) INSTALL_K3S_EXEC="--flannel-iface eth1" sh -s -
